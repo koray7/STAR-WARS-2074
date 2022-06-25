98f8003c8074b34cd6156e467fea4a7a89a9b67a
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-25 15:19:58 -0500

    API calls for resident

--- src/components/residentsPage/ResidentsName.js
+++ src/components/residentsPage/ResidentsName.js
@@ -1,23 +1,35 @@
-// import { useEffect, useState } from 'react'
+import { useState } from 'react'
 // import { useParams } from 'react-router-dom'
 // import { Link } from "react-router-dom";
 import Axios from "axios";
 
 
 function App() {
+
+const [resName, getResName] = useState("")
+
 const ResidentsName = () => {
 
-    Axios.get("https://swapi.dev/api/planets")
+    Axios.get(`https://swapi.dev/api/planets/`)
     .then((res) => {
-        console.log(res);
-    }
-    );
+        console.log(res.data)
+        getResName(res.climate)
+    }).catch(err => {
+        console.log(err)
+    })
+        
+    
 };
 
     return(
         <div>
             <h1>People</h1>
-            <button onClick={ResidentsName}>Get Joke Right Now!</button>
+            <button onClick={ResidentsName}>Get the residents now!</button>
+            
+            { resName ? <h1>{resName}</h1> : null}
+
+
+            {/* <Link to={`/planets/${planet.id}`}>{planet.name}</> */}
         </div>
     )
 }
