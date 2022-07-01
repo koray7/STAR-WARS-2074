7f075c80c2a1a5b4d9dfb998cad44254ece147d5
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-30 23:15:59 -0500

    Hamburger Button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { useState } from "react";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuToggler = () => setMenuOpen((p) => !p);
+
   return (
     <div className={styles.header}>
       <div className={styles.header__content}>
@@ -9,7 +15,9 @@ const Header = () => {
           <span className={styles.logo}>Star Wars</span>
         </div>
         <div>
-          <nav className={styles.nav}>
+          <nav
+            className={`${styles.nav} ${menuOpen ? styles[`nav--open`] : {}}`}
+          >
             <a className={styles.nav__item} href={"/"}>
               Planet Page
             </a>
@@ -19,15 +27,26 @@ const Header = () => {
             <a className={styles.nav__item} href={"/"}>
               Resident Details Page
             </a>
+            <div className={styles.nav__button__container}>
+              <Button />
+            </div>
           </nav>
         </div>
         <div>
-          <div className={styles.header__button__container}></div>
-          <button className={styles.header__toggler}></button>
+          <div className={styles.header__button__container}>
+            <Button />
+          </div>
+          <button className={styles.header__toggler} onClick={menuToggler}>
+            <BiMenuAltRight />
+          </button>
         </div>
       </div>
     </div>
   );
+};
+
+const Button = () => {
+  return <button className={styles.button}>Click me</button>;
 };
 
 export default Header;
---
+++ src/service.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export function PlanetNameService() {
+  return "xyz";
+}
